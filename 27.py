58e5524caae98b6ad067cfde5a6337ae5bc64641
Edson Borges <edsonn.borgess@gmail.com>
2016-07-07 12:37:03 -0300

    Update 27.py

--- 27.py
+++ 27.py
@@ -1,7 +1,6 @@
+#Definir tempo a partir dos segundos
 segundos=int(input("Segundos: "))
 hora = (segundos/3600)
 minutos = (segundos/60)%60
 segundo = (segundos%60)
-print (hora,"Horas ,%d minutos,%d segundos"%minutos %segundo)
-print minutos
-print segundo
+print ("%d Horas |%d minutos|%d segundos"%(hora,minutos,segundo))
