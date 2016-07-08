15e1d3cb147748afec92531954d4ca69b82d2c92
Edson Borges <edsonn.borgess@gmail.com>
2016-07-08 13:25:25 -0300

    Update 38.py

--- 38.py
+++ 38.py
@@ -1,10 +1,16 @@
 print("__#Soma de Fracao#__")
-deno1=int(input("Digite o denominador "))
 nume1=int(input("Digite o numerador "))
-print ("Fracao #1 %d/%d"%(deno1,nume1))
-deno2=int(input("Digite o denominador "))
+deno1=int(input("Digite o denominador "))
+print ("Fracao #1 %d/%d"%(nume1,deno1))
 nume2=int(input("Digite o numerador "))
-print ("Fracao #1 %d/%d"%(deno1,nume1))
-soma_deno = (deno1+deno2)
-soma_nume=(nume1+nume2)
-print ("%d/%d"%(soma_deno,soma_nume))
+deno2=int(input("Digite o denominador "))
+print ("Fracao #1 %d/%d"%(nume2,deno2))
+if deno1 == deno2:
+    soma_deno = (deno1+deno2)
+    soma_nume=(nume1+nume2)
+    print ("%d/%d"%(soma_deno,soma_nume))
+else:
+    soma_deno = ((deno1*nume2)+(deno2*nume1))
+    soma_nume = ((nume1*nume2))
+    print ("%d/%d"%(soma_deno,soma_nume))
+
